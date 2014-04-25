0722a269a26f49a8f84da8ddd43e81cb9f28d3cd
Michael Garvin <gar@andyet.net>
2014-04-25 16:44:07 -0700

    whitespace removal

--- config.js
+++ config.js
@@ -1,14 +1,9 @@
 {
-
   "host": "0.0.0.0",
   "port": 7777,
-
   "keyLength": 10,
-
   "staticMaxAge": 86400,
-
   "recompressStaticAssets": true,
-
   "logging": [
     {
       "level": "verbose",
@@ -16,18 +11,14 @@
       "colorize": true
     }
   ],
-
   "keyGenerator": {
     "type": "phonetic"
   },
-
   "storage": {
     "type": "file",
     "path": "/var/local/artifices"
   },
-
   "documents": {
     "about": "./about.md"
   }
-
 }
